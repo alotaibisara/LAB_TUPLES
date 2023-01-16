8aef617753f1462396ea4bb92bdf91aadab275a7
Sara Alotaibi <alotaibisara49@gmail.com>
2023-01-16 11:36:46 +0300

    done

---
+++ lab tuples.py
@@ -0,0 +1,16 @@
+numbers ="1","2","3",
+letters= "a","b","c",
+print (numbers [0])
+print (letters[-1])
+new_tuple = numbers+letters
+print (new_tuple)
+result_len = len(new_tuple)
+print (result_len)
+if "4" in new_tuple :
+    print ("4 in the result_len ")
+else : print( "4 not in the result_len") 
+list1 = [4,5,6]
+new_tuple2= tuple(list1)
+var1,var2,var3 = numbers
+var4,var5 ,var6 = letters
+print (var1)
